bab6b642e69de155d2cd7bb441d90790c1e098ab
mehmetsaidpelit <you@example.com>
2023-04-01 11:52:43 -0300

    4.gun

---
+++ src/G01.java
@@ -0,0 +1,30 @@
+import com.sun.tools.javac.Main;
+
+public class G01 {
+
+    public static void main(String[] args) {
+
+        /*
+        Git-GitHub'da projelerimizin uzerinde calisirken temelde 3 adimda islem yapiyoruz
+
+        1- Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz
+
+            git init
+
+        2- dosyamizda/projemizde bulunanlari versiyon olusturuyorduk
+
+             git add .
+             git commit -m"versiyonadi"
+
+        3- github'a yollama entegrasyonu
+
+            git remote add origin repoUrlAdresi
+            aldready origin
+            git remote set-url repoUrlAdresi
+
+            git push
+
+
+         */
+    }
+}
